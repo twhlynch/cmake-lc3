cefe639786495707bae8018eb6110403d8a57ada
Tom Lynch <twhlynch@gmail.com>
2026-09-17 20:41:26 +1000

    feat: add some useful constants to utils

--- src/lc3_utils.cmake
+++ src/lc3_utils.cmake
@@ -1,3 +1,19 @@
+# MARK: contants
+
+set(LC3_REGISTER_COUNT 8)
+
+set(LC3_BYTE_BITS 8)
+set(LC3_BYTE_MASK 0xFF)
+set(LC3_WORD_MASK 0xFFFF)
+
+set(LC3_ASCII_LIMIT 128)
+
+set(LC3_MEMORY_SIZE 65536) # x10000
+set(LC3_USER_MIN 12288) # x3000
+set(LC3_USER_MAX 65024) # xFE00
+
+# MARK: interactive
+
 #[[
 	Throws a FATAL_ERROR if the condition is not met.
 ]]
