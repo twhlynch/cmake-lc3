dd8781c86cb65127aab10a3409ff590b6123e51b
Tom Lynch <twhlynch@gmail.com>
2026-09-19 00:13:52 +1000

    chore: tests for and instruction

--- test/test_instructions.cmake
+++ test/test_instructions.cmake
@@ -10,5 +10,16 @@ vm_exec_add(4193) # add r0 r1 #1
 test_assert_equal("${R0}" "2" "add imm")
 
 test_reset()
+set(R1 6)
+set(R2 5)
+vm_exec_and(20546) # and r0 r1 r2
+test_assert_equal("${R0}" "4" "and")
+
+test_reset()
+set(R1 6)
+vm_exec_and(20579) # and r0 r1 #3
+test_assert_equal("${R0}" "2" "and imm")
+
+test_reset()
 vm_exec_trap(61477) # trap x25 (halt)
 test_assert_equal("${VM_HALT}" "1" "trap")
