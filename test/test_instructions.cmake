14a94ef0dd8ff4868b70920e57991bfb0a2b6597
Tom Lynch <twhlynch@gmail.com>
2026-09-18 23:59:28 +1000

    chore: test for add immediate

--- test/test_instructions.cmake
+++ test/test_instructions.cmake
@@ -3,3 +3,8 @@ set(R1 1)
 set(R2 1)
 vm_exec_add(4162) # add r0 r1 r2
 test_assert_equal("${R0}" "2" "add")
+
+test_reset()
+set(R1 1)
+vm_exec_add(4193) # add r0 r1 #1
+test_assert_equal("${R0}" "2" "add imm")
