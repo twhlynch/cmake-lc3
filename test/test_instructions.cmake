cc7c7cc99a89364c9a7d45c6a7466a2d0c373715
Tom Lynch <twhlynch@gmail.com>
2026-09-19 00:18:38 +1000

    chore: test for lea instruction

--- test/test_instructions.cmake
+++ test/test_instructions.cmake
@@ -67,6 +67,10 @@ set(MEM_12290 44)
 vm_exec_ldr(24702) # ldr r0 r1 #-2
 test_assert_equal("${R0}" "44" "ldr negative")
 
+test_reset()
+vm_exec_lea(57345) # lea r0 #1
+test_assert_equal("${R0}" "12289" "lea")
+
 
 test_reset()
 vm_exec_trap(61477) # trap x25 (halt)
